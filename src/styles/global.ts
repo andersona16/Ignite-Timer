ba466f120b6695ecc33d78aefdb15dac67254be7
Anderson Araujo <andersonaraujoc1@gmail.com>
2022-08-01 17:33:15 -0300

    Timer finalizado

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyled = createGlobalStyle`
     *
@@ -10,12 +10,13 @@ export const GlobalStyled = createGlobalStyle`
 
     :focus{
         outline: 0;
-        box-shadow: 0 0 0 2px ${(props) => props.theme['gray-500']};
+        box-shadow: 0 0 0 2px ${(props) => props.theme["gray-500"]};
     }
 
     body { 
-        background: ${(props) => props.theme['gray-900']};
-        color: ${(props) => props.theme['gray-300']};
+        background: ${(props) => props.theme["gray-900"]};
+        color: ${(props) => props.theme["gray-300"]};
+        -webkit-font-smoothing: antialiased;
     }
 
     body, input, textarea, button {
@@ -23,4 +24,4 @@ export const GlobalStyled = createGlobalStyle`
         font-weight: 400;
         font-size: 1rem;
     }
-`
+`;
